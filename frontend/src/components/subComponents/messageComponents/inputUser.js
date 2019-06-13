3ac609e267e1297ccad1f7cedf75afb9df3c009c
Kevin Yang <kvnyang503@gmail.com>
2019-06-12 22:46:18 -0700

    added an inputUser Component

--- frontend/src/components/subComponents/messageComponents/inputUser.js
+++ frontend/src/components/subComponents/messageComponents/inputUser.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route } from 'react-router-dom';
 
-const inputUser = () => {
+import '../../../styles/inputUser.css';
 
-    return (
-        <div className="inputUser field">
-            <label className="label">Name</label>
-            <div className="control">
-                <input className="input" type="text" placeholder="Input name here" />
+class inputUser extends Component {
+
+
+    render(){
+
+
+        return (
+            <div className="inputUser field">
+                {/* <label className="label">Name</label>
+                <div className="control">
+                    <input className="input" type="text" placeholder="Input name here" />
+                </div> */}
+
+                <div className="inputContainer">
+                    <span className="to">
+                        To:
+                    </span>
+                    {/* place card selectors in here */}
+                    <div className="contactChip">
+                        {/* <figure className="image is-32x32" id="ContactChipFigure">
+                            
+                            <img className="image is 32x32 is-rounded" src="https://bulma.io/images/placeholders/128x128.png" />
+                            
+                            
+                        </figure> */}
+                        usernamehere
+                        
+                        {/* add some kind of close btn here */}
+
+                    </div>
+
+                    <input className="inputUserField" type="text" /> {/* As something get's type, search for a user */}
+                </div>
+                
+
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default inputUser;
