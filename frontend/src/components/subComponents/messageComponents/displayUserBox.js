b950b092d9fb0a73987dca6647ea848c714ea301
Kevin Yang <kvnyang503@gmail.com>
2019-06-18 20:24:40 -0700

    created the userCardUserBox component

--- frontend/src/components/subComponents/messageComponents/displayUserBox.js
+++ frontend/src/components/subComponents/messageComponents/displayUserBox.js
@@ -4,11 +4,14 @@ import { withRouter, Route } from 'react-router-dom';
 
 import "../../../styles/displayUserBox.css";
 
+import UserCard from './userCardUserBox';
+
+//This is considered a container
 class displayUserBox extends Component {
     render(){
         return (
             <div className="box displayUserBox">
-                
+                <UserCard />
             </div>
         )
     }
---
+++ frontend/src/components/subComponents/messageComponents/userCardUserBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const userCardUserBox = (props) => {
+    
+    return (
+        <div>
+            <p>Display username here: {props.userName}</p>
+            <hr />
+        </div>
+    );
+};
+
+export default userCardUserBox;
