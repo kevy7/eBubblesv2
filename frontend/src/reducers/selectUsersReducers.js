5f06d383ebfe1d5302943fd4f48fe0b560fac39c
kevy7 <kvnyang503@gmail.com>
2019-06-23 23:10:46 -0700

    added a way to push value into the selectedUsers array for the selectUsersReducer

--- frontend/src/reducers/selectUsersReducers.js
+++ frontend/src/reducers/selectUsersReducers.js
@@ -5,6 +5,18 @@ const initialState = {
     selectedUsers: []
 }
 
+/*
+How to push data into an array
+
+case ADD_ITEM :
+    return { 
+        ...state,
+        arr: [...state.arr, action.newItem]
+    }
+
+
+*/
+
 const selectUsersReducer = (state=initialState, action) => {
 
     if(action.type === LOAD_COMPONENT){
@@ -16,7 +28,8 @@ const selectUsersReducer = (state=initialState, action) => {
     else if (action.type === SELECT_USERS){
         return {
             ...state,
-            loading: false
+            loading: false,
+            selectedUsers: [...state.selectedUsers, action.payload] //this should allow us to push data into our selectedUsers array using a redux reducer
             /* find out how to pass in a new value into your current selectedUsers array  */
         }
     }
