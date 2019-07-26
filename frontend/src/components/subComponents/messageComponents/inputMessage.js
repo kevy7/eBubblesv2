37fec6b099b065339f4778805091c335eb10b365
kevy7 <kvnyang503@gmail.com>
2019-07-25 23:04:06 -0700

    updated the inputMessage component.

--- frontend/src/components/subComponents/messageComponents/inputMessage.js
+++ frontend/src/components/subComponents/messageComponents/inputMessage.js
@@ -9,6 +9,7 @@ import { connect } from 'react-redux';
 import { withRouter, Route, Link } from 'react-router-dom';
 
 import "../../../styles/inputMessage.css";
+import { postMessage } from "../../../actions";
 
 
 
@@ -19,7 +20,6 @@ class inputMessage extends Component {
     }
 
     handleInputChanges = async (e) => {
-        //e.preventDefault();
 
         await this.setState({[e.target.name]: e.target.value});
         //console.log(this.state.userMessage)
@@ -28,15 +28,19 @@ class inputMessage extends Component {
     sendMessage = async (e) => {
         e.preventDefault();
 
-        //Send message to users
+        const messageData = {
+            authUserID: this.props.auth.userInfo.id,
+            messageID: this.props.selectedConversation.selectedConversation[0]._id,
+            message: this.state.userMessage,
+            authName: this.props.auth.userInfo.name
+        }
 
-        //Create a conversation if no conversation currently exists for these users
-        //action that will 
+        //if I post a message, it will redirect the user to a new page and we don't want that
+
 
 
         //If a conversation does exist for these users, then the send button shouldn't be creating another conversation
-
-        alert("you're clicking on a different button!!");
+        alert("Alert, you're clicking on a different button");
 
     }
 
@@ -68,10 +72,12 @@ class inputMessage extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        selectUsersReducers: state.selectUsersReducers
+        selectUsersReducers: state.selectUsersReducers,
+        selectedConversation: state.selectedConversation,
+        auth: state.auth
     }
 }
 
 export default withRouter(connect(mapStateToProps, {
-
+    postMessage
 })(inputMessage));
