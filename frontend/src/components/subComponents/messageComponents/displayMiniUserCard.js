452eb3e4c3e276db1f31b71496babc7fb936a3bf
kevy7 <kvnyang503@gmail.com>
2019-08-05 22:38:55 -0700

    import proptypes to miniusercard component

--- frontend/src/components/subComponents/messageComponents/displayMiniUserCard.js
+++ frontend/src/components/subComponents/messageComponents/displayMiniUserCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import propTypes from "prop-types";
 
 import MiniUserCard from "./miniUserCard";
 import { getUserProfile } from "../../../actions"
