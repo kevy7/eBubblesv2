897fa7a2c7afedb689c0ae10faeccb622e8a5b00
kevy7 <kvnyang503@gmail.com>
2020-08-27 21:37:22 -0700

    updated the authReducer

--- frontend/src/reducers/authReducer.js
+++ frontend/src/reducers/authReducer.js
@@ -1,10 +1,11 @@
 import authenticate from "../services/authenticate";
 
-import { SET_CURRENT_USER } from '../actions/types';
+import { SET_CURRENT_USER, LOAD_LOGIN_COMPONENT } from '../actions/types';
 
 const initialState ={
     isAuthenticated: false,
-    userInfo: {}
+    userInfo: {},
+    loading: false
 }
 
 //This reducer will run when the user is trying to login
@@ -13,7 +14,14 @@ export default (state = initialState, action) => {
         return {
             ...state,
             isAuthenticated: authenticate(), //authenticate is going to return true or false based on if we can decode the user's jwt token
-            userInfo: action.payload
+            userInfo: action.payload,
+            loading: false
+        }
+    }
+    else if (action.type === LOAD_LOGIN_COMPONENT){
+        return {
+            ...state,
+            loading: true
         }
     }
     else {
