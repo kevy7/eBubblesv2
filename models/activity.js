b8be7b34fbbda174b558f71924bdf197af0e52a2
kevy7 <kvnyang503@gmail.com>
2021-03-17 21:55:22 -0700

    added createdBy to activity

--- models/activity.js
+++ models/activity.js
@@ -3,7 +3,12 @@ var mongoose = require("mongoose");
 var activitySchema = new mongoose.Schema({
     activityName: String,
     activityDescription: String,
-    activityDate: Date
+    activityDate: Date,
+    createdby: {
+        //The user that created this event
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },  
 });
 
 module.exports = mongoose.model("Activity", activitySchema);
